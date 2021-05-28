17fb9fca452afc0af58d8998cf345df7e65c9e19
Dimas Cahyo <dimasamid557@gmail.com>
2021-05-28 07:12:12 +0000

    add laravel/ui to project # Add laravel/ui # Modify view layouts.app # Modify view home # Add ASSET_URL=asset to .env.example

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+@section('content-title', 'Dashboard')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-header">Dashboard</div>
+
+        <div class="card-body">
+          @if (session('status'))
+            <div class="alert alert-success" role="alert">
+              {{ session('status') }}
+            </div>
+          @endif
+
+          You are logged in!
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
